7430c85639446336fa9ed84f93a172428c6e10a4
Prince Christopher <pchristopher@fncinc.com>
2017-01-31 19:55:14 -0600

    update submodule

--- apps/TamilGreeter/index.js
+++ apps/TamilGreeter/index.js
@@ -53,7 +53,7 @@ var processMessage = function(body){
    var match = /<div\s+class="post-content">([\s\S]*?)<div\s+class="tweetable-content([\s\S]*?)<\/div>([\s\S]*?)<\/div>/gim;
    var meditationContent = match.exec(body);
     message = meditationContent[1].trim() + '</p>';
-    message = message + console.log(meditationContent[3]);       
+    message = message + meditationContent[3];       
     return message;
 }
 
